668618d77a18174cd87858737a836e633cb123fa
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-01-12 00:51:00 +0100

    Use abelsiqueira/GraphViz to draw graphs

--- graph-pure-julia.jl
+++ graph-pure-julia.jl
@@ -1,11 +1,23 @@
-using Colors, GitHub, Graphs, NetworkLayout, PkgDeps, Plots, Random, Statistics, LinearAlgebra
+using Colors, GitHub, Graphs, NetworkLayout, PkgDeps, Plots, Random, Statistics, LinearAlgebra, GraphViz
 pyplot()
+
+### You need abelsiqueira/GraphViz
 
 ignore_pkgs = ["JuliaSmoothOptimizers.github", "Organization"]
 model_pkgs = ["ADNLPModels", "AmplNLReader", "BundleAdjustmentModels", "CUTEst", "LLSModels", "ManualNLPModels", "NLPModels", "NLPModelsJuMP", "NLPModelsModifiers", "NLPModelsTest", "NLSProblems", "OptimizationProblems", "PDENLPModels", "QuadraticModels", "QPSReader"]
 solver_pkgs = ["BenchmarkProfiles", "CaNNOLeS", "DCISolver", "JSOSolvers", "NLPModelsIpopt", "NLPModelsKnitro", "Percival", "RipQP", "SolverCore", "SolverTest", "SolverTools", "SolverBenchmark"]
 la_pkgs = ["AMD", "BasicLU", "HSL", "Krylov", "LDLFactorizations", "LimitedLDLFactorizations", "LinearOperators", "PROPACK", "MUMPS", "QRMumps", "SparseMatricesCOO", "SuiteSparseMatrixCollection"]
-loose_pkgs = ["BasicLU", "MUMPS", "OptimizationProblems", "QRMumps", "SuiteSparseMatrixCollection"]
+
+colors = ["red", "darkgreen", "purple", "black"]
+class_of = pkg -> if pkg in model_pkgs
+        1
+    elseif pkg in solver_pkgs
+        2
+    elseif pkg in la_pkgs
+      3
+    else
+      4
+    end
 
 function download_stuff()
   jso_pkgs = GitHub.repos("JuliaSmoothOptimizers")
@@ -30,186 +42,71 @@ function download_stuff()
   return jso_pkgs, deps
 end
 
-function compute_positions(jso_pkgs, deps)
-  all_pkgs = (vcat(values(deps)...) ∪ jso_pkgs) |> unique |> sort
-  external = setdiff(all_pkgs, jso_pkgs)
-
-  n = length(all_pkgs)
-  G = SimpleDiGraph(n)
-  for (i,x) in enumerate(all_pkgs)
-    for y in get(deps, x, [])
-      j = findfirst(y .== all_pkgs)
-      add_edge!(G, i, j)
+function draw_graph(pkgs, deps, filename; engine="dot", limit_to_jso=false)
+  graphviz = """digraph {
+    size="30";
+    ratio="compress";
+    ranksep="1.5";
+    node [style=filled,fontcolor=white];
+  """
+  # Nodes
+  pkg_deps = reduce(
+    (acc, cur) -> acc ∪ cur,
+    [get(deps, pkg, []) for pkg in pkgs]
+  )
+  all_pkgs = (pkg_deps ∪ pkgs) |> unique |> sort
+  if limit_to_jso
+    all_pkgs = all_pkgs ∩ jso_pkgs
+  end
+  for pkg in all_pkgs
+    cls = class_of(pkg)
+    color = colors[cls]
+    shape = cls ≤ 3 ? "box" : "oval"
+    size = 25 + 2 * length(get(deps, pkg, 0))
+    graphviz *= "$pkg [label=$pkg, color=$color, shape=$shape, fontsize=$size];\n"
+  end
+  for pkg in pkgs, dep in get(deps, pkg, [])
+    if limit_to_jso && !(dep in jso_pkgs)
+      continue
     end
+    graphviz *= "$pkg -> $dep;"
   end
 
-  A = adjacency_matrix(G)
+  graphviz *= "}\n"
 
-  # Not great, but working
-  # layout = spring(
-  #   A,
-  #   # seed = 7,
-  #   # initialpos = [(i, i * 0.1) for i = 1:n]
-  # )
-  # posx = getindex.(layout, 1)
-  # posy = getindex.(layout, 2)
-
-  # Separate clusters
-  posx = zeros(n)
-  posy = zeros(n)
-  offset = [(0, 0), (-35, -6), (35, -6)]
-  for (k, pkg_set) in enumerate([model_pkgs, solver_pkgs, la_pkgs])
-    # Add external pkgs
-    neighs = vcat([
-      all_pkgs[neighbors(G, findfirst(pkg .== all_pkgs))] for pkg in pkg_set
-    ]...) |> unique |> sort
-    neighs = neighs ∩ external
-    pkg_set = pkg_set ∪ neighs
-
-    idxs = indexin(pkg_set, all_pkgs)
-    Aset = A[idxs, idxs]
-    layout = spring(
-      Aset,
-      iterations = 1000,
-      seed = 2,
-      # C = sqrt(length(pkg_set)) * 2.5,
-      C = 14,
-    )
-    x = getindex.(layout, 1)
-    y = getindex.(layout, 2)
-    posx[idxs] = x .+ offset[k][1]
-    posy[idxs] = y .+ offset[k][2]
+  open(pipeline(`dot -Tpng -K$engine`, filename), "w", stdout) do io
+    print(io, graphviz)
   end
 
-  layout = spring(
-    A,
-    initialpos = zip(posx, posy),
-    C = 10000,
-    iterations = 50,
-  )
-  posx = getindex.(layout, 1)
-  posy = getindex.(layout, 2)
-
-  return G, all_pkgs, posx, posy
-end
-
-function plot_graph(G, all_pkgs, posx, posy, jso_pkgs)
-  n = length(posx)
-
-  external = setdiff(all_pkgs, jso_pkgs)
-
-  classes = [
-    if pkg in model_pkgs
-        1
-    elseif pkg in solver_pkgs
-        2
-    elseif pkg in la_pkgs
-      3
-    else
-      4
-    end
-    for pkg in all_pkgs
-  ]
-  JL = Colors.JULIA_LOGO_COLORS
-  color_map = [JL.red, JL.green, JL.purple, JL.blue]
-  colors = color_map[classes]
-  neighbs = [neighbors(G, i) for i = 1:n]
-  text_size(w) = ceil(Int, 14 + w * 0.6)
-  text_sizes = [text_size(length(neighbs[i])) for i = 1:n]
-
-  function box_around!(x, y, ℓ, h, c)
-    plot!(
-      x .+ ℓ * [-1, 1, 1, -1, -1],
-      y .+ h * [-1, -1, 1, 1, -1],
-      c = :gray,
-      fill = true,
-      fillcolor = :white,
-      lab = "",
-      opacity = 0.8,
-    )
-  end
-
-  plt = plot(size=(1920, 1080), leg=false, grid=false, axis=false)
-  for i = 1:n, j = 1:n
-    if has_edge(G, i, j)
-      p = [posx[i]; posy[i]]
-      q = [posx[j]; posy[j]]
-      d = q - p
-      p = p + d / norm(d)
-      q = q - 2 * d / norm(d)
-      if classes[j] == 4
-        scatter!([p[1]], [p[2]], c=colors[i], ms=5)
-        plot!([p[1], q[1]], [p[2], q[2]], l=:arrow, lab="", c=:black, opacity=0.2, lw=2)
-      else
-        plot!([p[1], q[1]], [p[2], q[2]], l=:dash, lab="", c=:black, opacity=0.1, lw=1)
-      end
-    end
-  end
-
-
-  for i = sort(1:n, by=j -> text_sizes[j])
-    h = 0.07 * text_sizes[i]
-    ℓ = 0.025 * length(all_pkgs[i]) * text_sizes[i] + 0.08 * text_sizes[i]
-    box_around!(posx[i], posy[i], ℓ, h, colors[i])
-  end
-
-  annotate!(
-    [(posx[i], posy[i], text(all_pkgs[i], text_sizes[i], colors[i])) for i = 1:n],
-  )
-
-  # legends
-  legends = ["Models", "Solvers", "Linear Algebra", "External"]
-  x_leg = -60 * ones(4)
-  y_leg = 28 .- 3 * (0:3)
-  scatter!(
-    x_leg,
-    y_leg,
-    m = :square,
-    ms = 20,
-    c = color_map,
-    lab = "",
-  )
-
-  annotate!(
-    [(
-      x_leg[i] + length(legends[i]) * 0.4 + 2,
-      y_leg[i],
-      text(legends[i], 16)
-    ) for i = 1:4]
-  )
-
-  png("graph")
-  plt
+  GraphViz.Graph(graphviz, engine=engine)
 end
 
 function main(jso_pkgs, deps)
-  G, all_pkgs, posx, posy = compute_positions(jso_pkgs, deps)
-  indexof(pkg) = findfirst(pkg .== all_pkgs)
-  fix_list = [
-    ("BasicLU", 0, -5),
-    ("MRIsim", 0, -2),
-    ("LDLFactorizations", -10, 0),
-    ("Krylov", -10, 0),
-    ("MRFingerprintingRecon", 50, -10),
-    ("QDLDL", -2, -2),
-    ("ScHoLP", -10, -15),
-    ("MadNLPTests", 0, 20),
-    ("SparsityOperators", 0, -2),
-    ("Preconditioners", 3, 0),
-    ("StoppingInterface", 8, 13),
-    ("SparseMatricesCOO", 30, -3),
-    ("Cloudy", 25, 0),
-    ("CaNNOLeS", 3, 0),
-  ]
-  for (pkg, Δx, Δy) in fix_list
-    k = indexof(pkg)
-    posx[k] += Δx
-    posy[k] += Δy
+  @assert setdiff(jso_pkgs, model_pkgs ∪ solver_pkgs ∪ la_pkgs) == []
+
+  draw_graph(jso_pkgs, deps, "ecosystem.png", engine="dot")
+  draw_graph(jso_pkgs, deps, "ecosystem_only_jso.png", engine="dot", limit_to_jso=true)
+  draw_graph(["NLPModels"], deps, "nlpmodels.png", engine="fdp")
+  draw_graph(model_pkgs, deps, "models.png", engine="fdp")
+  draw_graph(["LinearOperators", "Krylov"], deps, "la_core.png", engine="fdp")
+  draw_graph(la_pkgs, deps, "linear_algebra.png", engine="fdp")
+  draw_graph(solver_pkgs, deps, "solvers.png", engine="fdp")
+
+  selected = String[]
+  S = ["LinearOperators", "Krylov"]
+  while length(S) > 0
+    pkg = pop!(S)
+    if pkg in selected
+      continue
+    end
+    push!(selected, pkg)
+    new_deps = get(deps, pkg, String[]) ∩ jso_pkgs
+    S = setdiff(S ∪ new_deps, selected)
   end
-  plot_graph(G, all_pkgs, posx, posy, jso_pkgs)
+  draw_graph(selected, deps, "selected.png", engine="dot", limit_to_jso=true)
 end
 
-# jso_pkgs, deps = download_stuff()
+jso_pkgs, deps = download_stuff()
 jso_union = sort(unique(model_pkgs ∪ solver_pkgs ∪ la_pkgs))
 if jso_pkgs != jso_union
   @info setdiff(jso_pkgs, jso_union)
