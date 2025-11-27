a23008adf873c994596ed5a5596561cbc15b6702
tmigot <tangi.migot@gmail.com>
2025-11-27 16:51:20 -0500

    Add action to update package list

--- pkgs_data/get_jso_packages.jl
+++ pkgs_data/get_jso_packages.jl
@@ -1,12 +1,43 @@
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.instantiate()
+
 using JSON
 
 function write_report(
-  organization_name::String = "JuliaSmoothOptimizers"
+    organization_name::String = "JuliaSmoothOptimizers";
+    filepath::String = "pkgs_data/list_jso_packages.dat",
 )
-  list_pkgs = organization_pkgs(organization_name)
-  open("pkgs_data/list_jso_packages.dat", "w") do io
-    [write(io, name * "\n") for name in list_pkgs]
-  end
+    # --- 1) Get current packages from GitHub ---
+    current_pkgs = Set(organization_pkgs(organization_name))
+
+    # --- 2) Read existing file if present ---
+    existing_pkgs = if isfile(filepath)
+        Set(readlines(filepath))
+    else
+        Set{String}()  # empty set
+    end
+
+    # --- 3) Compute differences ---
+    added   = current_pkgs  .- existing_pkgs
+    removed = existing_pkgs .- current_pkgs
+
+    # --- 4) Report differences ---
+    for pkg in added
+        @info "New package detected" pkg
+    end
+    for pkg in removed
+        @warn "Package in file not found in organization anymore" pkg
+    end
+
+    # --- 5) Write updated file (sorted for reproducibility) ---
+    open(filepath, "w") do io
+        for pkg in sort(collect(current_pkgs))
+            write(io, pkg, "\n")
+        end
+    end
+
+    return (added = added, removed = removed, current = current_pkgs)
 end
 
 # maximum 100 packages
@@ -19,3 +50,5 @@ function organization_pkgs(organization_name::String = "JuliaSmoothOptimizers")
   pkgs = map(x -> x[1:(end - 3)], pkgs)
   return sort(pkgs)
 end
+
+write_report()
